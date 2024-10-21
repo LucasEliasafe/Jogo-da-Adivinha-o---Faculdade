1e5113b8c6e49aea9cd8a558219c10c79c2144bd
Lucas Eliasafe Mohr Vargas <139092655+LucasEliasafe@users.noreply.github.com>
2024-10-21 17:59:51 -0300

    Feat: adicionando as variaveis para minimo, maximo e o número secreto

--- src/NumeroAleatorio.java
+++ src/NumeroAleatorio.java
@@ -1,2 +1,23 @@
-package PACKAGE_NAME;public class NumeroAleatorio {
+import java.util.Random;
+
+public class NumeroAleatorio {
+    private int numeroSecreto;
+    private int minimo;
+    private int maximo;
+
+    public NumeroAleatorio(int minimo, int maximo) {
+        this.minimo = minimo;
+        this.maximo = maximo;
+        gerarNumeroSecreto();
+    }
+
+    public void gerarNumeroSecreto() {
+        Random gerador = new Random();
+        this.numeroSecreto = random.nextInt((maximo - minimo) + 1) + minimo;
+    }
+
+    public int getNumeroSecreto() {
+        return numeroSecreto;
+    }
+
 }
