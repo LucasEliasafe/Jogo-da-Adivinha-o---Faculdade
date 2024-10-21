ca1ac6c9f4d1eb0fc86b8f77443b053b5ca53dfe
Lucas Eliasafe Mohr Vargas <139092655+LucasEliasafe@users.noreply.github.com>
2024-10-21 18:06:50 -0300

    Feat: Criado método para a adivinhação dos números definindo um minimo e maximo

--- src/JogoAdivinhacao.java
+++ src/JogoAdivinhacao.java
@@ -10,4 +10,41 @@ public class JogoAdivinhacao {
         this.tentativas = 0;
         this.jogoAtivo = true;
     }
+
+    public void jogar() {
+        Scanner scanner = new Scanner(System.in);
+        int chute;
+        while (jogoAtivo) {
+            System.out.print("Adivinhe o número (ou digite 0 para sair): ");
+            try {
+                chute = scanner.nextInt();
+
+                if (chute == 0) {
+                    System.out.println("Você saiu do jogo.");
+                    jogoAtivo = false;
+                } else {
+                    tentativas++;
+                    verificarChute(chute);
+                }
+            } catch (Exception e) {
+                System.out.println("Entrada inválida! Por favor, digite um número.");
+                scanner.next();  // Limpa o buffer
+            }
+        }
+        scanner.close();
+    }
+
+    private void verificarChute(int chute) {
+        int numeroSecreto = numeroAleatorio.getNumeroSecreto();
+
+        if (chute > numeroSecreto) {
+            System.out.println("O número secreto é menor.");
+        } else if (chute < numeroSecreto) {
+            System.out.println("O número secreto é maior.");
+        } else {
+            System.out.println("Parabéns! Você acertou o número em " + tentativas + " tentativas.");
+            jogoAtivo = false;
+        }
+    }
+
 }
