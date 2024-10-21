4d615fe4ff080ed9a5a94b6efc66b6b1e6024b54
Lucas Eliasafe Mohr Vargas <139092655+LucasEliasafe@users.noreply.github.com>
2024-10-21 18:19:48 -0300

    Feat: criado método para menu do jogo e definir os intervalos do jogo

--- src/Main.java
+++ src/Main.java
@@ -1,2 +1,19 @@
+import java.util.Scanner;
+
 public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Bem-vindo ao Jogo de Adivinhação de Números!");
+
+        System.out.print("Digite o valor mínimo do intervalo: ");
+        int minimo = scanner.nextInt();
+
+        System.out.print("Digite o valor máximo do intervalo: ");
+        int maximo = scanner.nextInt();
+
+        JogoAdivinhacao jogo = new JogoAdivinhacao(minimo, maximo);
+        jogo.jogar();
+
+        System.out.println("Jogo encerrado. Você realizou " + jogo.getTentativas() + " tentativas.");
+    }
 }
